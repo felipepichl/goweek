5cfcd6ffd7a20949a49dca88e9ef5c9bcc1214d2
Felipe <felipe.pichl@hotmail.com>
2021-03-24 15:58:16 -0300

    GoWeek-05- Done tweet in mongodb

--- goweek-05/server/src/modules/tweets/infra/typeorm/repositories/TweetsRepository.ts
+++ goweek-05/server/src/modules/tweets/infra/typeorm/repositories/TweetsRepository.ts
@@ -9,15 +9,13 @@ class TweetsRepository implements ITweetRepository {
   private ormRepository: MongoRepository<Tweet>;
 
   constructor() {
-    this.ormRepository = getMongoRepository(Tweet, 'mongo');
+    this.ormRepository = getMongoRepository(Tweet);
   }
 
   public async create({ user, content }: ICreateTweetDTO): Promise<Tweet> {
     const tweet = this.ormRepository.create({ user, content });
 
-    console.log(tweet);
-
-    // await this.ormRepository.save(tweet);
+    await this.ormRepository.save(tweet);
 
     return tweet;
   }
