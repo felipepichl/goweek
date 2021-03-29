c6bc6512c2710ec36c590684a2fe4045bace0f1f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-03-28 23:03:25 -0300

    GoWeek-05- Mobile - Add navigation

--- goweek-05/mobile/src/start/App.tsx
+++ goweek-05/mobile/src/start/App.tsx
@@ -1,15 +1,19 @@
 import { registerRootComponent } from 'expo';
+
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from '../routes';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" />
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello World</Text>
+        <Routes />
       </View>
-    </>
+    </NavigationContainer>
   );
 };
 
