e8fd539a3084e3fcb75849432097f74e4edaf7ce
Felipe <felipe.pichl@hotmail.com>
2021-04-15 12:12:35 -0300

    GoWeek-05- Set user in storage

--- goweek-05/web/src/context/AuthContext.tsx
+++ goweek-05/web/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
 
 interface ISignInCreadetial {
@@ -6,8 +6,13 @@ interface ISignInCreadetial {
 }
 
 interface IAuthContextData {
-  name: string;
+  user: object;
   signIn(credentials: ISignInCreadetial): Promise<void>;
+}
+
+interface IAuthState {
+  token: string;
+  user: object;
 }
 
 export const AuthContext = createContext<IAuthContextData>(
@@ -15,16 +20,32 @@ export const AuthContext = createContext<IAuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<IAuthState>(() => {
+    const token = localStorage.getItem('@GoWeek-05:token');
+    const user = localStorage.getItem('@GoWeek-05:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as IAuthState;
+  });
+
   const signIn = useCallback(async ({ login }) => {
     const response = await api.post('sessions', {
       login,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoWeek-05:token', token);
+    localStorage.setItem('@GoWeek-05:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Felipe', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
