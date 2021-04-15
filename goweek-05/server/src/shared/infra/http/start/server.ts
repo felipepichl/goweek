42d83444f5b0b3166820dc500fb3bac3991f34b3
Felipe <felipe.pichl@hotmail.com>
2021-04-15 12:21:14 -0300

    GoWeek-05- Add Cors

--- goweek-05/server/src/shared/infra/http/start/server.ts
+++ goweek-05/server/src/shared/infra/http/start/server.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata';
 
-import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import AppError from '@shared/errors/AppError';
 import routes from '@shared/infra/http/routes';
@@ -11,6 +12,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
