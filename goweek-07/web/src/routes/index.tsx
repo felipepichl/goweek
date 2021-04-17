e95d24782fb4b5e9570b42ab169b0c8007154ff4
Felipe <felipe.pichl@hotmail.com>
2021-04-17 17:38:36 -0300

    GoWeek-07- Add web

---
+++ goweek-07/web/src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import Home from '../pages/Home';
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SignIn} />
+        <Route exact component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
