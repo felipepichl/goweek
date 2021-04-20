0e983fd54994bc414d47a46010cbbc2452b8a852
Felipe Pichl <felipe.pichl@hotmail.com>
2021-04-19 21:36:14 -0300

    GoWeek-05- Done authenticated routes

--- goweek-05/web/src/routes/index.tsx
+++ goweek-05/web/src/routes/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Home from '../pages/Home';
@@ -9,7 +11,7 @@ const Routes: React.FC = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={SignIn} />
-        <Route path="/home" component={Home} />
+        <Route path="/home" component={Home} isPrivate />
       </Switch>
     </BrowserRouter>
   );
