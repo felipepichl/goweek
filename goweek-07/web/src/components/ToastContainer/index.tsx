e23f2b4b3d4be92752aef6eda5911ec919154309
Felipe Pichl <felipe.pichl@hotmail.com>
2021-04-29 22:35:45 -0300

    GoWeek-07- Done ToastContainer, css

--- goweek-07/web/src/components/ToastContainer/index.tsx
+++ goweek-07/web/src/components/ToastContainer/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { FiCircle, FiXCircle } from 'react-icons/fi';
 
 import { Container, Toast } from './styles';
 
 const ToastContainer: React.FC = () => {
   return (
     <Container>
-      <Toast>
+      <Toast hasDescription>
+        <FiCircle size={20} />
+
         <div>
           <strong>An error ocurred</strong>
+          <p>You cannot log in</p>
         </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
       </Toast>
     </Container>
   );
--- goweek-07/web/src/components/ToastContainer/styles.ts
+++ goweek-07/web/src/components/ToastContainer/styles.ts
@@ -1,4 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ToastProps {
+  type?: 'info' | 'success' | 'error';
+  hasDescription: boolean;
+}
+
+const toastTypeVariations = {
+  info: css`
+    background: #ebf8ff;
+    color: #3172b7;
+  `,
+
+  success: css`
+    background: #e6fffa;
+    color: #26656a;
+  `,
+
+  error: css`
+    background: #fddede;
+    color: #c53030;
+  `,
+};
 
 export const Container = styled.div`
   position: absolute;
@@ -8,7 +30,7 @@ export const Container = styled.div`
   overflow: hidden;
 `;
 
-export const Toast = styled.div`
+export const Toast = styled.div<ToastProps>`
   width: 360px;
 
   position: relative;
@@ -21,4 +43,40 @@ export const Toast = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props => toastTypeVariations[props.type || 'info']}
+
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 16px;
+    top: 19px;
+    opacity: 0.6;
+    border: 0;
+    background: transparent;
+    color: inherit;
+  }
+
+  ${props =>
+    !props.hasDescription &&
+    css`
+      align-items: center;
+      svg {
+        margin-top: 0;
+      }
+    `}
 `;
