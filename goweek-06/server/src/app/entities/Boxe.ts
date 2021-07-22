92e6a0c54e63c16844a4bf099ce100017233f429
Felipe Pichl <felipe.pichl@hotmail.com>
2021-07-22 20:58:24 -0300

    GoWeek-06- Boxes entity

---
+++ goweek-06/server/src/app/entities/Boxe.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+import { User } from './User';
+
+@Entity('boxes')
+class Boxes {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  user_id: string;
+
+  @JoinColumn({ name: 'user_id' })
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Boxes };
