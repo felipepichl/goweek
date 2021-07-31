862317902073f36cfce59843a11c45f921923e4b
Felipe <felipe.pichl@hotmail.com>
2021-07-31 17:33:02 -0300

    GoWeek-06- Files routes

---
+++ goweek-06/server/src/app/routes/file.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import { CreateFilesController } from '@controller/Files/CreateFilesController';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
+const createFileRouter = Router();
+const upload = multer(uploadConfig);
+
+const createFilesController = new CreateFilesController();
+
+createFileRouter.use(ensureAuthenticated);
+
+createFileRouter.post('', upload.single('file'), createFilesController.handle);
+
+export { createFileRouter };
--- goweek-06/server/src/routes/index.ts
+++ goweek-06/server/src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { createUserRouter } from '../app/routes/user.routes';
 import { sessionRouter } from '../app/routes/session.routes';
 import { createBoxeRouter } from '../app/routes/boxe.routes';
+import { createFileRouter } from '../app/routes/file.routes';
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.use('/users', createUserRouter);
 router.use('/session', sessionRouter);
 
 router.use('/boxes', createBoxeRouter);
+
+router.use('/files', createFileRouter);
 
 export default router;
