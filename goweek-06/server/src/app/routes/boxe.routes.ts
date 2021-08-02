f42d3a6c14685791ab3f0de76b285f8ef600167f
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-01 22:49:28 -0300

    GoWeek-06- Fix Boxes routes

--- goweek-06/server/src/app/routes/boxe.routes.ts
+++ goweek-06/server/src/app/routes/boxe.routes.ts
@@ -1,22 +1,18 @@
 import { Router } from 'express';
 
-import multer from 'multer';
-import uploadConfig from '@config/upload';
-
 import { CreateBoxesController } from '../controllers/Boxes/CreateBoxesController';
 import { ListBoxesController } from '../controllers/Boxes/ListBoxesController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const createBoxeRouter = Router();
-const upload = multer(uploadConfig);
 
 const createBoxesController = new CreateBoxesController();
 const listBoxesController = new ListBoxesController();
 
 createBoxeRouter.use(ensureAuthenticated);
 
-createBoxeRouter.post('', upload.single('file'), createBoxesController.handle);
+createBoxeRouter.post('', createBoxesController.handle);
 createBoxeRouter.get('', listBoxesController.handle);
 
 export { createBoxeRouter };
