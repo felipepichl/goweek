10df37f05d29555728bc2c2f84a706fbeaac34a5
Felipe Pichl <felipe.pichl@hotmail.com>
2021-08-01 23:09:06 -0300

    GoWeek-06- Fix Create File/Box

--- goweek-06/server/src/app/services/File/CreateFilesServices.ts
+++ goweek-06/server/src/app/services/File/CreateFilesServices.ts
@@ -3,25 +3,40 @@ import { getCustomRepository, Repository } from 'typeorm';
 import { File } from '@entities/File';
 import { FileRepository } from 'app/repositories/FileRepository';
 
+import { Boxe } from '@entities/Boxe';
+import { BoxesRepository } from 'app/repositories/BoxesRepository';
+
 interface IRequest {
   title: string;
   path: string;
+  box_id: string;
 }
 
 class CreateFilesServices {
   private filesRepository: Repository<File>;
 
+  private boxesRepository: Repository<Boxe>;
+
   constructor() {
     this.filesRepository = getCustomRepository(FileRepository);
+    this.boxesRepository = getCustomRepository(BoxesRepository);
   }
 
-  public async execute({ title, path }: IRequest): Promise<File> {
+  public async execute({ title, path, box_id }: IRequest): Promise<File> {
     const file = this.filesRepository.create({
       title,
       path,
     });
 
     await this.filesRepository.save(file);
+
+    const boxe = await this.boxesRepository.findOne({
+      where: { id: box_id },
+    });
+
+    boxe.file_id = file.id;
+
+    this.boxesRepository.save(boxe);
 
     return file;
   }
