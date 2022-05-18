05b634af31b64998be05852975b2e1c74a394973
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-17 23:25:59 -0300

    GoWeek-08:Web: Doing Main html

--- goweek-08/version-1.0/web/src/pages/Main/index.js
+++ goweek-08/version-1.0/web/src/pages/Main/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import FadeIn from 'react-fade-in';
-import { Container } from './styles';
+
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+
+import { Container, Buttons } from './styles';
 
 export default function Main({ match }) {
   return (
     <Container>
       <FadeIn delay={85} transitionDuration={600}>
-        <h1>{match.params.id}</h1>
+        <img src={logo} alt="Tindev" />
+
+        <ul>
+          <li>
+            <img
+              src="https://avatars.githubusercontent.com/u/2254731?v=4"
+              alt=""
+            />
+
+            <footer>
+              <strong>Diego Fernandes</strong>
+              <p>
+                "CTO at @Rocketseat. Passionate about education and changing
+                people's lives through programming.
+              </p>
+            </footer>
+            <Buttons>
+              <button type="button">
+                <img src={dislike} alt="" />
+              </button>
+              <button type="button">
+                <img src={like} alt="" />
+              </button>
+            </Buttons>
+          </li>
+        </ul>
       </FadeIn>
     </Container>
   );
--- goweek-08/version-1.0/web/src/pages/Main/styles.js
+++ goweek-08/version-1.0/web/src/pages/Main/styles.js
@@ -8,18 +8,6 @@ export const Container = styled.div`
   width: 100vw;
   min-width: 320px;
   height: 100vh;
-
-  > div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    width: 90%;
-    height: 100px;
-    padding: 10px;
-    border-radius: 4px;
-    background: #fff;
-    opacity: 0.5;
-    font-size: 10rem;
-  }
 `;
+
+export const Buttons = styled.div``;
