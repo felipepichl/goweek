f4167b05a5e76574bda2f9b343814559f8f70c41
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-19 22:51:19 -0300

    GoWeek-08:Web: Done Main css

--- goweek-08/version-1.0/web/src/pages/Main/styles.js
+++ goweek-08/version-1.0/web/src/pages/Main/styles.js
@@ -29,9 +29,41 @@ export const Container = styled.div`
         padding: 15px 20px;
         text-align: left;
         border-radius: 0 0 5px 5px;
+
+        strong {
+          font-size: 16px;
+          color: #333;
+        }
+
+        p {
+          font-size: 14px;
+          line-height: 20px;
+          color: #999;
+          margin-top: 5px;
+        }
       }
     }
   }
 `;
 
-export const Buttons = styled.div``;
+export const Buttons = styled.div`
+  margin-top: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+
+  button {
+    height: 50px;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05);
+    border: 0;
+    border-radius: 4px;
+    background: #fff;
+
+    :hover {
+      img {
+        transform: translateY(-5px);
+        transition: all 0.2s;
+      }
+    }
+  }
+`;
