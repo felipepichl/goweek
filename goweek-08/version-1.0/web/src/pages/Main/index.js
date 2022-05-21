879dfd6645670bf957ef23c1eabe81db0b3d977c
Felipe Pichl <felipe.pichl@hotmail.com>
2022-05-20 22:11:25 -0300

    GoWeek-08:Web: get devs from api

--- goweek-08/version-1.0/web/src/pages/Main/index.js
+++ goweek-08/version-1.0/web/src/pages/Main/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FadeIn from 'react-fade-in';
+import { api } from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import like from '../../assets/like.svg';
@@ -8,103 +9,45 @@ import dislike from '../../assets/dislike.svg';
 import { Container, Buttons } from './styles';
 
 export default function Main({ match }) {
+  const { id } = match.params;
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs', {
+        headers: { user: id },
+      });
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, [id]);
   return (
     <Container>
       <FadeIn delay={85} transitionDuration={600}>
         <img src={logo} alt="Tindev" />
 
         <ul>
-          <li>
-            <img
-              src="https://avatars.githubusercontent.com/u/2254731?v=4"
-              alt=""
-            />
+          {devs.map(dev => (
+            // eslint-disable-next-line no-underscore-dangle
+            <li key={dev._id}>
+              <img src={dev.avatar} alt="" />
 
-            <footer>
-              <strong>Diego Fernandes</strong>
-              <p>
-                "CTO at @Rocketseat. Passionate about education and changing
-                people's lives through programming.
-              </p>
-            </footer>
-            <Buttons>
-              <button type="button">
-                <img src={dislike} alt="" />
-              </button>
-              <button type="button">
-                <img src={like} alt="" />
-              </button>
-            </Buttons>
-          </li>
-
-          <li>
-            <img
-              src="https://avatars.githubusercontent.com/u/2254731?v=4"
-              alt=""
-            />
-
-            <footer>
-              <strong>Diego Fernandes</strong>
-              <p>
-                "CTO at @Rocketseat. Passionate about education and changing
-                people's lives through programming.
-              </p>
-            </footer>
-            <Buttons>
-              <button type="button">
-                <img src={dislike} alt="" />
-              </button>
-              <button type="button">
-                <img src={like} alt="" />
-              </button>
-            </Buttons>
-          </li>
-
-          <li>
-            <img
-              src="https://avatars.githubusercontent.com/u/2254731?v=4"
-              alt=""
-            />
-
-            <footer>
-              <strong>Diego Fernandes</strong>
-              <p>
-                "CTO at @Rocketseat. Passionate about education and changing
-                people's lives through programming.
-              </p>
-            </footer>
-            <Buttons>
-              <button type="button">
-                <img src={dislike} alt="" />
-              </button>
-              <button type="button">
-                <img src={like} alt="" />
-              </button>
-            </Buttons>
-          </li>
-
-          <li>
-            <img
-              src="https://avatars.githubusercontent.com/u/2254731?v=4"
-              alt=""
-            />
-
-            <footer>
-              <strong>Diego Fernandes</strong>
-              <p>
-                "CTO at @Rocketseat. Passionate about education and changing
-                people's lives through programming.
-              </p>
-            </footer>
-            <Buttons>
-              <button type="button">
-                <img src={dislike} alt="" />
-              </button>
-              <button type="button">
-                <img src={like} alt="" />
-              </button>
-            </Buttons>
-          </li>
+              <footer>
+                <strong>{dev.name}</strong>
+                <p>{dev.bio}</p>
+              </footer>
+              <Buttons>
+                <button type="button">
+                  <img src={dislike} alt="" />
+                </button>
+                <button type="button">
+                  <img src={like} alt="" />
+                </button>
+              </Buttons>
+            </li>
+          ))}
         </ul>
       </FadeIn>
     </Container>
